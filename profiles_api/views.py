c741c042404334860391ed325bfc29a158e74300
Aleksandra <aleksandra.markovic@marbleit.rs>
2022-07-29 13:44:09 +0200

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from profiles_api import serializers
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+
+from profiles_api import models
 
 
 class HelloApiView(APIView):
@@ -93,3 +97,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
